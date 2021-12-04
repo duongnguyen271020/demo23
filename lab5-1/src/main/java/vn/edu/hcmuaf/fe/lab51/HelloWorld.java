8e9cd7735aada17f81e12bc26aaff1e0993da0da
duongnguyen271020 <92619808+duongnguyen271020@users.noreply.github.com>
2021-12-04 22:55:48 +0700

    update
    source updating

---
+++ lab5-1/src/main/java/vn/edu/hcmuaf/fe/lab51/HelloWorld.java
@@ -0,0 +1,20 @@
+package vn.edu.hcmuaf.fe.lab51;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HelloWorld", value = "/HelloWorld")
+public class HelloWorld extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+     doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    //show hello world
+        response.getWriter().println("hello world");
+    }
+}
